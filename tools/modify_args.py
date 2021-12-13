955acf6ab835b25950da4e0751cb5d88d222cc4b
jerryzz668 <59353310+jerryzz668@users.noreply.github.com>
2021-12-13 11:58:55 +0800

    Update modify_args.py

--- tools/modify_args.py
+++ tools/modify_args.py
@@ -2,15 +2,15 @@
 
 def modify_args(args):
 
-    args.input_size = (1088, 4000)
-    args.test_size = (1088, 4000)
+    args.input_size = (1088, 4000)  # (h,w)
+    args.test_size = (1088, 4000)  # (h,w)
     args.num_classes = 2
     args.eval_interval = 1
     args.print_interval = 20
-    args.data_dir = '/home/jerry/data/Micro_A/A_loushi/10-23-loushi/labelme/labelme_gj_update_bbaug_coco'
+    args.data_dir = '/home/jerry/data_coco'
     args.train_ann = "instances_train2017.json"
     args.val_ann = "instances_val2017.json"
-    args.max_epoch = 250
+    args.max_epoch = 300
     args.no_aug_epochs = 15
 
     return args
